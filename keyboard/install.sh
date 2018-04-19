cc876692dafbfa548d560e35a00cf038693eb4b6
mndvns <mail@mndvns.com>
2018-04-18 19:07:33 -0600

    clean up karabiner

--- keyboard/install.sh
+++ keyboard/install.sh
@@ -1,1 +1,6 @@
-ln -fs  $DOTFILES/keyboard/karabiner.json $HOME/.config/karabiner/karabiner.json
+# https://pqrs.org/osx/karabiner/document.html#configuration-file-path
+
+test -e $HOME/.config/karabiner && rm -rf $HOME/.config/karabiner
+ln -s  $DOTFILES/keyboard/karabiner $HOME/.config/karabiner
+
+launchctl kickstart -k gui/`id -u`/org.pqrs.karabiner.karabiner_console_user_server
